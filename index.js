0ec49bbdbba06b4590653996f863058cdf5bd8c5
Jascha Ephraim <jascha@jaschaephraim.com>
2017-12-07 11:52:57 -0800

    Update rules

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ module.exports = {
     browser: true,
     jest: true,
   },
+  globals: {
+    Web3: true,
+  },
   rules: {
     'function-paren-newline': 'off',
     'no-console': 'off',
+    'no-param-reassign': 'off',
     'no-underscore-dangle': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true
+      }
+    ],
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
