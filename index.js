317050b4bf30d466d9b9a5d8736a10235616a878
Kieran O'Reilly <kieran@gifs.com>
2017-12-30 17:58:46 -0500

    Update max length of line

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ module.exports = {
     Web3: true,
   },
   rules: {
+    'max-len': [
+      'error',
+      {
+        code: 200
+      }
+    ],
     'function-paren-newline': 'off',
     'no-console': 'off',
     'no-param-reassign': 'off',
