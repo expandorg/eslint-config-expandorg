d867ec84a9c1442c6f70650eb98a141a2d273770
Jascha Ephraim <jascha@jaschaephraim.com>
2018-10-05 11:36:48 -0700

    Split configs by environment

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   extends: [
-    'airbnb',
-    'plugin:flowtype/recommended',
-    'prettier',
-    'prettier/flowtype',
-    'prettier/react',
+    "airbnb",
+    "plugin:flowtype/recommended",
+    "prettier",
+    "prettier/flowtype"
   ],
   env: {
-    browser: true,
     es6: true,
-    jest: true,
+    jest: true
   },
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': ['off'],
-    'class-methods-use-this': ['off'],
-    'no-param-reassign': ['off'],
-    'object-curly-newline': ['off'],
-    'lines-between-class-members': ['off'],
-    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-    'react/button-has-type': ['off'],
-    'react/destructuring-assignment': ['off'],
-    'no-underscore-dangle': ['off'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        specialLink: ['hrefLeft', 'hrefRight'],
-        apects: ['noHref', 'invalidHref', 'preferButton'],
-      },
-    ],
-    'react/prefer-stateless-function': ['off'],
-    'react/prop-types': [2, { ignore: ['children'] }],
+    "prettier/prettier": "error",
+    "no-console": ["off"],
+    "class-methods-use-this": ["off"],
+    "no-param-reassign": ["off"],
+    "object-curly-newline": ["off"],
+    "lines-between-class-members": ["off"],
+    "no-underscore-dangle": ["off"],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }]
   },
-  plugins: ['react', 'prettier', 'flowtype'],
+  plugins: ["prettier", "flowtype"]
 };
---
+++ react.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["@gemsorg/eslint-config", "prettier/react"],
+  env: { browser: true },
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
+    "react/button-has-type": ["off"],
+    "react/destructuring-assignment": ["off"],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        specialLink: ["hrefLeft", "hrefRight"],
+        apects: ["noHref", "invalidHref", "preferButton"]
+      }
+    ],
+    "react/prefer-stateless-function": ["off"],
+    "react/prop-types": [2, { ignore: ["children"] }]
+  },
+  plugins: ["react"]
+};
