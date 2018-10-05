9e8f621b5cfedd614b9defd5ce9b8b98b8807b90
Slava Sobolev <sobolevslava@gmail.com>
2018-10-05 15:02:07 +0800

    flow + prettier

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 module.exports = {
-  extends: require.resolve('eslint-config-airbnb'),
-  env: {
-    browser: true,
-    jest: true,
-  },
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+  ],
   globals: {
     Web3: true,
   },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
   rules: {
-    'function-paren-newline': 'off',
-    'no-console': 'off',
-    'no-param-reassign': 'off',
-    'no-underscore-dangle': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true
-      }
-    ],
+    'prettier/prettier': 'error',
+    'no-console': ['off'],
+    'class-methods-use-this': ['off'],
+    'no-param-reassign': ['off'],
+    'object-curly-newline': ['off'],
+    'lines-between-class-members': ['off'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'react/button-has-type': ['off'],
+    'react/destructuring-assignment': ['off'],
+    'no-underscore-dangle': ['off'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
-        specialLink: [ 'hrefLeft', 'hrefRight' ],
-        apects: [ 'noHref', 'invalidHref', 'preferButton' ],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        apects: ['noHref', 'invalidHref', 'preferButton'],
       },
     ],
+    'react/prefer-stateless-function': ['off'],
+    'react/prop-types': [2, { ignore: ['children'] }],
   },
+  plugins: ['react', 'prettier', 'flowtype'],
 };
